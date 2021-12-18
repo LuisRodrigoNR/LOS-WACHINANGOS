e2d560b8328282e5c0766491aa9cedc2b2e30826
Adrian-Esteban-Santiago <92044199+Adrian-Esteban-Santiago@users.noreply.github.com>
2021-12-17 23:28:04 -0600

    Update SUMA.h

--- SUMA.h
+++ SUMA.h
@@ -1,14 +1,1 @@
-#include<stdio.h>
-
-int main ()
-{
-	int x,y, resultado;
-	
-	printf("Ingrese el primer numero: ");
-	scanf("%i", &x);
-	printf("Ingrese el segundo numero: ");
-	scanf("%i", &y);
-	resultado= x+y;
-	printf("El resultado de la suma es %i", resultado);
-	return 0;
-}
+int SUMA (int x, int y);
