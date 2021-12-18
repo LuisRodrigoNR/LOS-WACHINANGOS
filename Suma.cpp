807c8d7b5448a8f3b4b35cf69f642af3ec6900d0
Adrian-Esteban-Santiago <92044199+Adrian-Esteban-Santiago@users.noreply.github.com>
2021-12-17 23:26:44 -0600

    Update Suma.cpp

--- Suma.cpp
+++ Suma.cpp
@@ -1,14 +1,8 @@
 #include<stdio.h>
 
-int main ()
+int SUMA (int x, int y)
 {
-	int x,y, resultado;
-	
-	printf("Ingrese el primer numero: ");
-	scanf("%i", &x);
-	printf("Ingrese el segundo numero: ");
-	scanf("%i", &y);
-	resultado= x+y;
-	printf("El resultado de la suma es %i", resultado);
-	return 0;
+	int resultado;
+	resultado=x+y;
+	return resultado;
 }
