f3141580d296282747225ec98d6a4733fd28041c
AmeyalliReyes <yalgamesli.666@gmail.com>
2021-12-18 02:23:07 -0600

    libreria para dividir

--- Division.h
+++ Division.h
@@ -1,32 +1,14 @@
-//Programa que calcula divisiones de numeros reales 
-
 #include <stdio.h>
 
-int main()
+int dividir(float numerador, float denominador)
+
 {
-    float numerador; 
-    float denominador; 
-    float resultado; 
+	float resultado;
 
-    printf(" ==========Programa para calcular divisiones==========\n"); 
-
-    printf("\n>>Introduce los valores a dividir "); 
-    printf("\n>Numerador: ");
-    scanf_s("%f", &numerador);
-    printf(">Denominador: "); 
-    scanf_s("%f", &denominador); 
-
-    if (denominador != 0)
-    {
-        resultado = numerador / denominador;
-
-        printf("\n>>Resultado: ");
-        printf("\n%5.2f / %5.2f = %5.2f", numerador, denominador, resultado);
-    }
-    else
-        printf("\n Error: El valor del denominador debe ser un numero diferente de cero!!"); 
-
-    return 0; 
+	if (denominador != 0)
+	{
+		resultado = numerador / denominador;
+	}
+	return resultado;
 }
-
 
