6bf956701e84ba15f08b0bfdc624f2f5a1e3f037
Monse <91902542+Monse707@users.noreply.github.com>
2021-12-20 21:34:41 -0600

    Add files via upload

---
+++ Resta.h
@@ -0,0 +1,17 @@
+#include  <iostream>
+#include  <conio.h>
+
+	int main()
+	{
+		int a, b, resta;
+		std::cout << "\n ingrese numero a:";
+		std::cin >> a;
+		std::cout <<  "\n ingrese numerob:";
+		std::cin >> b;
+		resta = a -b;
+		std::cout << "\n la resta de "<< a <<"-" << b << "es:"<< resta;
+
+
+		return 0;
+
+}
